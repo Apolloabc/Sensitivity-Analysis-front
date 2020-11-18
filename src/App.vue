d67a85c0b651bd0fae474567a5fcac25f7e137cf
Apolloabc <1565916523@qq.com>
2020-11-18 10:59:59 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{}
+  },
+  mounted(){
+    this.axios({
+      method: "post",
+      url: "/SA-back-project/test",
+      data: this.qs.stringify({
+        username:'jj',
+        password:'jj'
+      })
+    }).then((res)=>{
+      console.log(res.data)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
